fe5aa5e99eabfca00f3b4b1a573b0c597b5a3a77
DaniManas <danimanas28@gmail.com>
2026-03-29 13:50:42 -0400

    Remove unrelated Documents folder from repository

--- Documents/Portfolio/src/controllers/CharacterAnimator.ts
+++
@@ -1,6 +0,0 @@
-// CharacterAnimator is no longer used.
-// Candle and orb animations are handled by UIController.
-export class CharacterAnimator {
-    constructor() {}
-    public destroy() {}
-}
